84bc49a596f817fb1882ec8fc82287ab20c22c78
gege <germano2227@gmail.com>
2024-04-22 11:39:58 -0300

    adicoes

--- setup/settings.py
+++ setup/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galeria'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
